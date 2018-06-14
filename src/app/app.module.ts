759c795bac8d75b100e1baef05fdede0275d862a
OSCAR ANDRES CESPEDES DIAZ <oscar.cespedes@correo.policia.gov.co>
2018-06-13 20:55:11 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './global/header/header.component';
+import { MenuComponent } from './global/menu/menu.component';
+import { CanastaComponent } from './global/canasta/canasta.component';
+import { HomeComponent } from './global/pages/home/home.component';
+import { appRouter } from './routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    CanastaComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRouter
   ],
   providers: [],
   bootstrap: [AppComponent]
